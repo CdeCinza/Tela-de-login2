db286187a26e14401f7ecf17ce821bf02600bac2
Grongos <87789087+CdeCinza@users.noreply.github.com>
2021-11-18 00:31:10 -0300

    Add files via upload

---
+++ XESQUEDELE/js/javascript.js
@@ -0,0 +1,8 @@
+//recebendo a resposta do prompt, e salvando na resposta da variavel 
+    var resposta = prompt ("qual é a senha ?")
+
+    //Verifica se a resposta é diferente (!=) de "132", se for diferente repete a rotina
+    while (resposta != "132"){
+        //abre a tela interativa para poder colocar a senha novamente, ele está salvando o valor que ele recebe do prompt, na variavel resposta
+    resposta =  prompt ("qual é a senha ?")
+    }
